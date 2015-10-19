971242dce4a3705770b367927df142504fe461d2
Callum Snowden <uberdum05@gmail.com>
2015-10-19 18:12:38 +0100

    Trying to get strncpy() working

--- ESP_WiFiScan_Display/ESP_WiFiScan_Display.ino
+++ ESP_WiFiScan_Display/ESP_WiFiScan_Display.ino
@@ -5,15 +5,18 @@ extern "C" {
 #include "user_interface.h"
 }
 
+const char* CURRENT_SSID_CHAR_FULL; //Full SSID
+char* CURRENT_SSID_CHAR; //Truncated SSID
+      
 void setup() {
   Serial.begin(115200);
   delay(1000);
-  Serial.print("Heap: "); Serial.println(system_get_free_heap_size());
+  Serial.print("Heap: "); Serial.println(system_get_free_heap_size()); //Print some basic information
   Serial.print("Boot version: "); Serial.println(system_get_boot_version());
   Serial.print("CPU frequency: "); Serial.println(system_get_cpu_freq());
   Serial.println();
 
-  Wire.pins(0, 2);
+  Wire.pins(0, 2); //Configure and enable I2C
   Wire.begin();
 
   startOLED();
@@ -25,8 +28,10 @@ void setup() {
 void loop() {
   clearDisplay();
   WiFi.mode(WIFI_STA);
-  WiFi.disconnect();
+  WiFi.disconnect(); //Disconnect from any AP, just to be sure
+  
   delay(100);
+  
   int NETWORKS_FOUND = WiFi.scanNetworks();
   Serial.println("WiFi network scan complete");
   Serial.print("No. of networks found: "); Serial.println(NETWORKS_FOUND);
@@ -34,16 +39,18 @@ void loop() {
   if (NETWORKS_FOUND != 0) {
     for (int i = 0; i < NETWORKS_FOUND; i++) {
       if (i > 5) {
-        break;
+        break; //We only want 5 on the OLED
       }
 
-      String CURRENT_SSID_STRING = WiFi.SSID(i);
-      char* CURRENT_SSID_CHAR;
+      CURRENT_SSID_CHAR_FULL = WiFi.SSID(i); //Copy to variable
+
+      Serial.println(CURRENT_SSID_CHAR_FULL); //Prints SSID fine
       
-      //CURRENT_SSID_STRING.toCharArray(CURRENT_SSID_CHAR, CURRENT_SSID_STRING.length());
+      os_strncpy(CURRENT_SSID_CHAR, CURRENT_SSID_CHAR_FULL, 15); //This is where the code fails with an exception, trying to truncate the string to 15 chars
       
-      Serial.print("SSID #"); Serial.print(i); Serial.print(": "); Serial.println(WiFi.SSID(i));
+      Serial.print("SSID #"); Serial.print(i); Serial.print(": "); Serial.println(CURRENT_SSID_CHAR);
       //sendStrXY(CURRENT_SSID_CHAR, i, 0);
+      delay(10);
     }
   }
 
